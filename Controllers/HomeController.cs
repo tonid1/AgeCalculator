f165b6bcee3893675d81e8d567494a6c6a36b8c5
Toni Dedić <dedictoni1@gmail.com>
2020-07-02 17:14:33 +0200

    Add project files.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,91 @@
+﻿using AgeCalculator.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AgeCalculator.Controllers
+{
+    public class HomeController : Controller
+    {
+        [HttpGet]
+        public ActionResult Index()
+        {
+            return View(new DatumRodjenja());
+        }
+
+        [HttpPost]
+        public ActionResult Index(DatumRodjenja datumRodj)
+        {
+            if (datumRodj.Datum > DateTime.Now)
+            {
+                ViewBag.Error = "Datum rodjenja ne moze biti veci od trenutnog datuma.";
+            }
+            else
+            {
+                ViewBag.Uvod = "Od Vaseg datuma rodjenja do danasnjeg dana proslo je: ";
+
+                int godine = (int.Parse(DateTime.Now.Year.ToString()) - int.Parse(datumRodj.Datum.Year.ToString())) * 12;
+                int mjeseci = int.Parse(DateTime.Now.Month.ToString()) - int.Parse(datumRodj.Datum.Month.ToString());
+
+                ViewBag.StarostMj = "U mjesecima: " + (godine + mjeseci) + " mjeseci,";
+
+                TimeSpan starost = DateTime.Now - datumRodj.Datum;
+
+                ViewBag.StarostDan = "U danima: " + Math.Round(starost.TotalDays) + " dana,";
+                ViewBag.StarostSat = "U satima: " + Math.Round(starost.TotalHours) + " sati,";
+                ViewBag.StarostMin = "U minutama: " + Math.Round(starost.TotalMinutes) + " minuta,";
+                ViewBag.StarostSek = "U sekundama: " + Math.Round(starost.TotalSeconds) + " sekunda";
+            }
+
+            return View(datumRodj);
+        }
+
+        [HttpGet]
+        public ActionResult DatePicker()
+        {
+            return View(new DatumRodjenja());
+        }
+
+        [HttpPost]
+        public ActionResult DatePicker(DatumRodjenja datumRodj)
+        {           
+            if (datumRodj.Datum > DateTime.Now)
+            {
+                ViewBag.Error = "Datum rodjenja ne moze biti veci od trenutnog datuma.";
+            }
+            else
+            {
+                ViewBag.Uvod = "Od Vaseg datuma rodjenja do danasnjeg dana proslo je: ";
+
+                int godine = (int.Parse(DateTime.Now.Year.ToString()) - int.Parse(datumRodj.Datum.Year.ToString())) * 12;
+                int mjeseci = int.Parse(DateTime.Now.Month.ToString()) - int.Parse(datumRodj.Datum.Month.ToString());
+
+                ViewBag.StarostMj = "U mjesecima: " + (godine + mjeseci) + " mjeseci,";
+
+                TimeSpan starost = DateTime.Now - datumRodj.Datum;
+
+                ViewBag.StarostDan = "U danima: " + Math.Round(starost.TotalDays) + " dana,";
+                ViewBag.StarostSat = "U satima: " + Math.Round(starost.TotalHours) + " sati,";
+                ViewBag.StarostMin = "U minutama: " + Math.Round(starost.TotalMinutes) + " minuta,";
+                ViewBag.StarostSek = "U sekundama: " + Math.Round(starost.TotalSeconds) + " sekunda";
+            }
+
+            return View(datumRodj);
+        }        
+
+        [HttpGet]
+        public async Task<ActionResult> Ajax()
+        {
+            return View(new DatumRodjenja());
+        }
+
+        [HttpPost]
+        public ActionResult Ajax(DatumRodjenja datumRodj)
+        {
+            return Json(datumRodj , JsonRequestBehavior.AllowGet);
+        }
+    }
+}
---
+++ Models/DatumRodjenja.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace AgeCalculator.Models
+{
+    public class DatumRodjenja
+    {
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:dd/MM/yyyy}", ApplyFormatInEditMode = true)]
+        public DateTime Datum { get; set; }
+    }
+}
